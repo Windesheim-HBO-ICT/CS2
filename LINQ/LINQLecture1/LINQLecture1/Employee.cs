609fd740dd6af591b3eb1b8959cfccfca571682e
Hieke Keuning <hw.keuning@windesheim.nl>
2017-10-10 09:53:58 +0200

    add

--- LINQ/LINQLecture1/LINQLecture1/Employee.cs
+++ LINQ/LINQLecture1/LINQLecture1/Employee.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace LINQLecture1
 {
-    class Employee
+
+    // Fig. 9.3: Employee.cs
+    public class Employee
     {
+        private decimal monthlySalaryValue;
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Employee(string first, string last, decimal salary)
+        {
+            FirstName = first;
+            LastName = last;
+            MonthlySalary = salary;
+        }
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalaryValue;
+            } // end get
+            set
+            {
+                if (value >= 0M) // if salary is non-negative
+                {
+                    monthlySalaryValue = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("{0,-10} {1,-10} {2,10:C}",
+               FirstName, LastName, MonthlySalary);
+        }
     }
+
+    /**************************************************************************
+     * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+     * Pearson Education, Inc. All Rights Reserved.                           *
+     *                                                                        *
+     * DISCLAIMER: The authors and publisher of this book have used their     *
+     * best efforts in preparing the book. These efforts include the          *
+     * development, research, and testing of the theories and programs        *
+     * to determine their effectiveness. The authors and publisher make       *
+     * no warranty of any kind, expressed or implied, with regard to these    *
+     * programs or to the documentation contained in these books. The authors *
+     * and publisher shall not be liable in any event for incidental or       *
+     * consequential damages in connection with, or arising out of, the       *
+     * furnishing, performance, or use of these programs.                     *
+     *************************************************************************/
+
 }
--- LINQ/LINQLecture1/LINQLecture1/Program.cs
+++ LINQ/LINQLecture1/LINQLecture1/Program.cs
@@ -10,6 +10,26 @@ namespace LINQLecture1
     {
         static void Main(string[] args)
         {
+            var values = new[] { 2, 9, 5, 0, 3, 7, 1, 4, 8, 5 };
+
+            // ...
+            
+            Employee[] employees = {
+                 new Employee( "Jason", "Red", 5000M ),
+                 new Employee( "Ashley", "Green", 7600M ),
+                 new Employee( "Matthew", "Indigo", 3587.5M ),
+                 new Employee( "James", "Indigo", 4700.77M ),
+                 new Employee( "Luke", "Indigo", 6200M ),
+                 new Employee( "Jason", "Blue", 3200M ),
+                 new Employee( "Wendy", "Brown", 4236.4M )
+            };
+
+            // ...
+
+            var items = new List<string>
+            {
+                "aQua", "RusT", "yElLow", "rEd"
+            };
         }
     }
 }
